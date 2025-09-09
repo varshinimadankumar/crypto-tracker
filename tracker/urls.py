f67edc085d14fb2bc76bce1eeabecc9737172efa
Varshini M <varshinimadankumar@gmail.com>
2025-09-09 23:49:44 +0530

    Render-ready deployment setup with homepage and template

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'tracker'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # root of this app
+    path('', views.home, name='home'),
     path('api/prices/', views.api_prices, name='api_prices'),
     path('api/chart/<int:crypto_id>/', views.api_chart, name='api_chart'),
     path('alerts/create/', views.create_alert, name='create_alert'),
