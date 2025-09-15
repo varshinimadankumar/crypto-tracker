c3fd451e9975baebfd0b5dab8f2a29885a0df8a0
Varshini M <varshinimadankumar@gmail.com>
2025-09-15 20:20:04 +0530

    Added static site files in docs/

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.contrib import admin
+from tracker import views
 from . import views
 
 app_name = 'tracker'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('api/prices/', views.api_prices, name='api_prices'),
-    path('api/chart/<int:crypto_id>/', views.api_chart, name='api_chart'),
-    path('alerts/create/', views.create_alert, name='create_alert'),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("api/prices/", views.api_prices, name="api_prices"),
+    path("api/chart/<int:crypto_id>/", views.api_chart, name="api_chart"),
+    path("alerts/create/", views.create_alert, name="create_alert"),
 ]
